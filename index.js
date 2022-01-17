4f6953bca22609394b01341fb7aa160828b6ad32
Larry Motuzis <larrymotuzis@gmail.com>
2022-01-17 13:27:12 -0600

    commit tree e4bc04069d2168152c0edbe2180664ab177d5c87

--- index.js
+++ index.js
@@ -1,34 +1,17 @@
-// for (let age = 30; age < 40; age++) {
-//     console.log(`I'm ${age} years old. Happy birthday to me!`);
-// }
 
-// const gifts = ['teddy bear', 'drone', 'doll'];
+function writeCards(arrayOfNames) {
+    console.log('HELLLOOOOOO', arrayOfNames)
+    let newArray = [];
+    for(let i = 0; i < arrayOfNames.length; i++) {
+        let thanksMessage = `Thank you, ${arrayOfNames[i]}, for the wonderful surprise gift!`
+        newArray.push(thanksMessage);
 
-// function wrapGifts(gifts)    {
-//     for (let i = 0; i < gifts.length; i++)  {
-//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
-//     }
-//     return gifts;
-// }
-
-// wrapGifts(gifts);
-
-
-function writeCards(friends)   {
-    let cardArray = []
-    for (let i = 0; i < friends.length; i++) {
-        console.log(`Thank you, ${friends[i]}, for the wonderful surprise gift!`);
-        cardArray.push(`Thank you, ${friends[i]}, for the wonderful surprise gift!`)
     }
-    return cardArray;
+    return newArray;
 }
 
-
-function countDown(number) {
-    while (number >= 0) {
-        console.log(number)
-        number--
+const countDown = (numbers) => {
+    while (numbers >= 0) {
+        console.log(numbers--)
     }
-   
 }
-
